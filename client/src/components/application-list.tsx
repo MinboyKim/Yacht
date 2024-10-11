8e7bc92e1035d8d4d24a2653c8edbaeb708c1960
MinboyKim <kdm3801@gmail.com>
2024-10-11 16:54:50 +0900

    feat: loading skeleton
    Implement applications loading skeleton

---
+++ client/src/components/application-list.tsx
@@ -0,0 +1,104 @@
+import { fetchClient } from "@/lib/fetch-client";
+import { CheckCircle, Ellipsis, LayoutGrid, Plus, Users } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const ApplicationList = async ({ projectId }: { projectId: string }) => {
+  const response = await fetchClient("/projects", {
+    method: "GET",
+  });
+  if (!response.ok) {
+    return (
+      <div className="h-full w-full p-6 text-center text-red-500">Error</div>
+    );
+  }
+  const projects = await response.json();
+  const selectedProject = projects.find(
+    (project) => project.projectId === +projectId,
+  );
+  const applications = selectedProject?.applications || [];
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold md:text-4xl">
+          {selectedProject?.projectName}
+        </h1>
+        <div className="flex items-center gap-4">
+          <Button variant="secondary" className="flex items-center gap-4">
+            <Users size={24} />
+            Invite
+          </Button>
+          <Button asChild className="flex items-center gap-4">
+            <Link href={`/projects/${projectId}/create`}>
+              <Plus size={24} className="hidden md:block" />
+              Create Application
+            </Link>
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex h-full w-full flex-col gap-8">
+        {applications.length === 0 && (
+          <div className="flex h-full w-full items-center justify-center text-2xl text-gray-500">
+            No applications
+          </div>
+        )}
+        {applications.map((application) => (
+          <Link
+            key={application.id}
+            href={`/projects/${projectId}/application/${application.id}`}
+          >
+            <div className="flex w-full cursor-pointer flex-col gap-4 rounded-xl border p-8 hover:bg-gray-300 dark:hover:bg-gray-700">
+              <div className="flex items-center gap-4">
+                <LayoutGrid size={20} />
+                <h2 className="text-2xl font-bold">{application.name}</h2>
+              </div>
+
+              <p className="text-gray-500">Description</p>
+
+              <div className="flex flex-col items-center justify-between gap-10 md:flex-row">
+                <div className="flex items-center gap-4 rounded-xl border p-4 text-green-600">
+                  <CheckCircle size={20} />
+                  <h3 className="text-xl font-semibold">Prepare</h3>
+                </div>
+
+                <Ellipsis
+                  size={30}
+                  className="rotate-90 transform md:rotate-0"
+                />
+
+                <div className="flex items-center gap-4 rounded-xl border p-4">
+                  <CheckCircle size={20} />
+                  <h3 className="text-xl font-semibold">Build</h3>
+                </div>
+
+                <Ellipsis
+                  size={30}
+                  className="rotate-90 transform md:rotate-0"
+                />
+
+                <div className="flex items-center gap-4 rounded-xl border p-4">
+                  <CheckCircle size={20} />
+                  <h3 className="text-xl font-semibold">Deploy</h3>
+                </div>
+
+                <Ellipsis
+                  size={30}
+                  className="rotate-90 transform md:rotate-0"
+                />
+
+                <div className="flex items-center gap-4 rounded-xl border p-4">
+                  <CheckCircle size={20} />
+                  <h3 className="text-xl font-semibold">Done</h3>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ApplicationList;
